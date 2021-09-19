58549839e8b470f2d793025cd1c47e5fc8752062
Prx001 <munichbayern2005@gmail.com>
2021-09-19 20:09:17 +0430

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name='QShowPasswordButton',
+	version='1.0.0',
+	packages=['QShowPasswordButton'],
+	url='https://github.com/Prx001/QShowPasswordButton',
+	license='MIT',
+	author='Parsa',
+	author_email='munichbayern2005@gmail.com',
+	description='A customized button to change the visibility of password characters'
+)
